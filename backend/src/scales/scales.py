9f984b2cb889e4e0816215e632c3179c15f0b5eb
Pablo Asensio Martínez <44872361+PabloAsensio@users.noreply.github.com>
2023-01-18 18:57:00 +0100

    Backend/algorithm/orientation (#12)
    * Backup for kitti... need to continue. #9
    
    * Backup for kitti... need to continue. #9
    
    * Vkitti2: Added user estimation. #9
    
    * Eurocmac: Added gt position. #9
    
    * Close #9

--- backend/src/scales/scales.py
+++ backend/src/scales/scales.py
@@ -3,28 +3,17 @@ import numpy as np
 from .grep import grep
 from .find_timestamp import find_timestamp
 
-
-def get_absolute_scale_kitti(groundtruth: list, frame_id: int) -> tuple:
-
-    ss = groundtruth[frame_id-1].strip().split()
-    x_prev = float(ss[3])
-    y_prev = float(ss[7])
-    z_prev = float(ss[11])
-
-    ss = groundtruth[frame_id].strip().split()
-    x = float(ss[3])
-    y = float(ss[7])
-    z = float(ss[11])
-    
-    true_t = np.array([x, y, z], dtype=np.float32).flatten().round(decimals=5)
-
-    return (np.sqrt((x - x_prev) * (x - x_prev) + (y - y_prev) * (y - y_prev) + (z - z_prev) * (z - z_prev))), true_t
-
-
 def get_absolute_scale_euromav(groundtruth: list, timestamp_groundtruth_list: list, frame_timestamps_list:list, frame_id: int) -> tuple:
 
     if frame_id == 0:
-        return 1., np.array([0., 0., 0.]).flatten()
+        finded = find_timestamp(timestamp_groundtruth_list, frame_timestamps_list[0])
+        line = grep(finded, groundtruth)
+
+        ss = line.split(',')
+        x = float(ss[1])
+        y = float(ss[3])
+        z = float(ss[2])
+        return 1., np.array([x, y, z]).flatten()
 
     finded = find_timestamp(timestamp_groundtruth_list, frame_timestamps_list[frame_id])
     line = grep(finded, groundtruth)
@@ -41,6 +30,22 @@ def get_absolute_scale_euromav(groundtruth: list, timestamp_groundtruth_list: li
     x = float(ss[1])
     y = float(ss[3])
     z = float(ss[2])
+    
+    true_t = np.array([x, y, z], dtype=np.float32).flatten().round(decimals=5)
+
+    return (np.sqrt((x - x_prev) * (x - x_prev) + (y - y_prev) * (y - y_prev) + (z - z_prev) * (z - z_prev))), true_t
+    
+def get_absolute_scale_kitti(groundtruth: list, frame_id: int) -> tuple:
+
+    ss = groundtruth[frame_id-1].strip().split()
+    x_prev = float(ss[3])
+    y_prev = float(ss[7])
+    z_prev = float(ss[11])
+
+    ss = groundtruth[frame_id].strip().split()
+    x = float(ss[3])
+    y = float(ss[7])
+    z = float(ss[11])
     
     true_t = np.array([x, y, z], dtype=np.float32).flatten().round(decimals=5)
 
--- backend/src/utils/messages.py
+++ backend/src/utils/messages.py
@@ -5,6 +5,23 @@ import cv2
 import numpy as np
 from src import VisualOdometry
 
+ROTATION_CAMERA_TO_WORLD = np.array([
+    [0, 0, 1],
+    [0, 1, 0],
+    [1, 0, 0]
+])
+
+INVERT_X_AXIS = np.array([
+    [-1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+])
+
+INVERT_Z_AXIS = np.array([
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, -1]
+])
 
 def encode64(img):
     frame = cv2.imencode('.JPEG', img)[1]
@@ -13,32 +30,44 @@ def encode64(img):
 
 async def create_message(vo: VisualOdometry, img: np.ndarray, img_id: int):
 
-    cur_t = vo.cur_t
-    if img_id > 2:
+    cur_t, true_t = vo.cur_t, vo.true_t
+    true_euler_angles = vo.get_true_euler_angles()
+    estimated_euler_angles = vo.get_estimated_euler_angles()
+
+    if img_id > 1:
+        # Rotate t vector dependin on dataset
+        if vo.dataset == 'kitti':
+            cur_t = ROTATION_CAMERA_TO_WORLD @ cur_t
+            true_t = ROTATION_CAMERA_TO_WORLD @ (true_t - vo.init_t)
+
+        if vo.dataset == 'vkitti2':
+            cur_t = ROTATION_CAMERA_TO_WORLD @ cur_t
+            true_t = INVERT_X_AXIS @ ROTATION_CAMERA_TO_WORLD @ (true_t - vo.init_t)
+
+        if vo.dataset == 'eurocmav':
+            cur_t = ROTATION_CAMERA_TO_WORLD @ cur_t
+            tmpPosition = (true_t - vo.init_t)
+            true_t = ROTATION_CAMERA_TO_WORLD @ (true_t - vo.init_t)
+            true_t *= 4
+
         x, y, z = cur_t[0][0], cur_t[1][0], cur_t[2][0]
-        if vo.dataset == "kitti":
-            pass
-            # x, z = z, x
+
     else:
-        x, y, z = 0.0, 0.0, 0.0
+        x, y, z = true_t[0], true_t[1], true_t[2]
 
-    trueX = vo.true_t[0]
-    trueY = vo.true_t[1]
-    trueZ = vo.true_t[2]
+    trueX = true_t[0]
+    trueY = true_t[1]
+    trueZ = true_t[2]
 
-    trueYaw, truePitch, trueRoll = vo.get_true_euler_angles()
-    estimatedYaw, estimatedPitch, estimatedRoll = vo.get_estimated_euler_angles()
-
-    # print("\tImage id: ", img_id)
-    # print("\t\tTrue Angles: ", trueYaw, truePitch, trueRoll)
-    # print("\t\tEstimated Angles: ", estimatedYaw, estimatedPitch, estimatedRoll)
+    trueRoll, truePitch, trueYaw = true_euler_angles
+    estimatedRoll, estimatedPitch, estimatedYaw = estimated_euler_angles
 
     message = {
         "img_id": img_id,
         "pose": {
-            "x": str(z),
+            "x": str(x),
             "y": str(y),
-            "z": str(x),
+            "z": str(z),
             "yaw":   str(estimatedYaw),
             "pitch": str(estimatedPitch),
             "roll":  str(estimatedRoll)
