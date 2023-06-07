04e61279464d79b38f5057dda680783f48119a44
Pablo Asensio Martinez <p.asensio97@gmail.com>
2023-06-07 16:30:43 +0200

    Update minor changes

--- backend/src/scales/__init__.py
+++ backend/src/scales/__init__.py
@@ -1,5 +1,5 @@
 from .read_eurocmav_timestamp_groundtruth import \
     read_eurocmav_timestamp_groundtruth
-from .scales import (get_absolute_scale_euromav, get_absolute_scale_kitti,
+from .scales import (get_absolute_scale_eurocmav, get_absolute_scale_kitti,
                      get_absolute_scale_vkitti2)
 from .true_rotation import (get_true_rotation_eurocmav, get_true_rotation_kitti, get_true_rotation_vkitti2)
